17be0221da6dae909b17eb18521cb92dc51359a8
JFAquinoPando <pandolcik@gmail.com>
2025-08-13 19:34:15 -0300

    Ahora voy a desplegar esta web

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    content: ["*.html"],
+    theme: {
+      extend: {
+        colors: {
+        
+        },
+        fontFamily: {
+          barlow: ["Barlow Semi Condensed", "sans-serif"],
+        },
+        fontWeight: {
+          500: 500,
+        },
+      },
+    },
+  };
+  
